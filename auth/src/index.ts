148bf72da33418b10fe21fe343b464804ce2f1d9
Aaryan Mehta <aaryanmehta2699@gmail.com>
2023-04-27 15:38:00 +0530

    completed deployment file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth"); // mongodb://auth-srv:27017/auth -> auth-mongo-srv is the service name, refer auth-mongo-depl.yaml
+    await mongoose.connect(process.env.MONGO_URI); // mongodb://auth-srv:27017/auth -> auth-mongo-srv is the service name, refer auth-mongo-depl.yaml
     console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
