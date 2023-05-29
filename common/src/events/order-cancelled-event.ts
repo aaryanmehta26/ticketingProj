e7fbdcb183aeebbde611427f9efb80e7ad8ca9ad
aaryanmehta26 <aaryanmehta2699@gmail.com>
2023-05-29 22:41:43 +0530

    Updates

--- common/src/events/order-cancelled-event.ts
+++ common/src/events/order-cancelled-event.ts
@@ -5,6 +5,7 @@ export interface OrderCancelledEvent {
     subject: Subjects.OrderCancelled;
     data: {
         id: string; // order id
+        version: number;
         ticket: {
             id: string;
         }
